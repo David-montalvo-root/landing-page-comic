ba7ee10bb14210f4bb56d6290c56cb994a1543a4
David Montalvo <davidmontalvo211@gmail.com>
2020-12-16 18:44:27 -0500

    Add files via upload

---
+++ Comic web/js/index.js
@@ -0,0 +1,26 @@
+window.sr = ScrollReveal();
+
+  sr.reveal('.texto-cabeza', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.album-content', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+
+
+
+
+
+
+  /*
+ top = arriba
+ right = derecha
+ bottom = abajo
+ left = izquierda
+  */
